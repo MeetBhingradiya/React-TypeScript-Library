4731f77d4ce1df728c2fd656421506ff95c654c3
MEET BHINGRADIYA <meetbhingradiya36@gmail.com>
2022-12-18 08:25:06 +0530

    @TeamSM/RTLT Init 2

--- Core/index.ts
+++ Core/index.ts
@@ -1,6 +1,36 @@
 // ! Export all modules from Core
 export * from './settings';
+import { Commands } from './commands';
 
-const ArguMents = process.argv;
+function ArguMents_Handler() {
+    const ArguMents = process.argv;
 
-console.log(ArguMents);
+    for (let index = 2; index < 4; index++) {
+        const ArguMent = ArguMents[index]
+
+        if (ArguMent.match('#') || ArguMent.match('#')) {
+            Commands.forEach((Command_) => {
+                Command_.alias.forEach((Alias) => {
+                    if (ArguMent === `${Command_.prefix}${Alias}`) {
+                        console.log('Command Found', ArguMent); // ! Fucntion Add Heare for Command
+                    }
+                });
+            });
+        } else if (ArguMent.includes('%') || ArguMent.includes('%')) {
+            Commands.forEach((Command_) => {
+                Command_.alias.forEach((Alias) => {
+                    if (ArguMent === `${Command_.prefix}${Alias}`) {
+                        console.log('Template Found', ArguMent); // ! Fucntion Add Heare for Template
+                    }
+                });
+            });
+        } else {
+            console.log('Command or Template Not Found'); // ! Fucntion Add Heare for Error Throw
+        }
+    }
+}
+
+
+// ! Debugger
+console.clear();
+ArguMents_Handler();
