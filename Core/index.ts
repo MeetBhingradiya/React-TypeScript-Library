59f17ae2aa40df4249285d18234fcf4612773795
MEET BHINGRADIYA <meetbhingradiya36@gmail.com>
2022-12-20 18:52:28 +0530

    @TeamSM/RTLT README.md Updated

--- Core/index.ts
+++ Core/index.ts
@@ -2,8 +2,10 @@
 export * from './settings';
 import { Commands } from './commands';
 
-function ArguMents_Handler() {
+function ArguMents_Handler(): Array<Function> {
     const ArguMents = process.argv;
+
+    const cmds: Array<Function> = [];
 
     for (let index = 2; index < 4; index++) {
         const ArguMent = ArguMents[index]
@@ -13,7 +15,9 @@ function ArguMents_Handler() {
                 Command_.alias.forEach((Alias) => {
                     if (ArguMent === `${Command_.prefix}${Alias}`) {
                         console.log('Command Found', ArguMent); // ! Fucntion Add Heare for Command
+                        cmds.push(Command_.function);
                     }
+                    return cmds;
                 });
             });
         } else if (ArguMent.includes('%') || ArguMent.includes('%')) {
@@ -21,13 +25,29 @@ function ArguMents_Handler() {
                 Command_.alias.forEach((Alias) => {
                     if (ArguMent === `${Command_.prefix}${Alias}`) {
                         console.log('Template Found', ArguMent); // ! Fucntion Add Heare for Template
+                        cmds.push(Command_.function);
                     }
+                    return cmds;
                 });
             });
         } else {
-            console.log('Command or Template Not Found'); // ! Fucntion Add Heare for Error Throw
+            return cmds;
         }
     }
+}
+
+function Execute() {
+    const commands = ArguMents_Handler();
+
+    if (commands.length === 0) {
+        console.log('No Commands Found');
+        process.exit(1);
+    }
+    
+
+    commands.forEach(async (command) => {
+        await command();
+    })
 }
 
 
